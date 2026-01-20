4b6ef1935744f9b011e7783360356605cd1e870a
Gaurav Phuyal <phuyalgaurav90@gmail.com>
2026-01-20 16:27:55 +0545

    initialized authContext

---
+++ context/AuthContext.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { useContext, createContext, ReactNode } from "react";
+
+interface AuthProps {
+  authState: { authenticated: boolean | null; users: any };
+  onRegister: (email: string, password: string) => Promise<any>;
+  onLogin: (email: string, password: string) => Promise<any>;
+  onLogout: () => Promise<any>;
+}
+
+const AuthContext = createContext<AuthProps>({} as AuthProps);
+
+export const useAuth = () => useContext(AuthContext);
+
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+  //auth logic here
+};
